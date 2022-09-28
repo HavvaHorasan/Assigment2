489ab09e62f360d4aa702c22af37ed777f6a1236
Havva <Havva@192.168.1.36>
2022-09-28 20:06:03 +0300

    2.HAFTA

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+// classların en temel özelliği ortak operasyonları tutmak
+	public static void main(String[] args) {
+		DortIslem dortislem= new DortIslem();
+		int sonuc = dortislem.Topla(3, 4);
+		System.out.println(sonuc);
+
+	}
+
+}
